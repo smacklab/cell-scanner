aa606245e803e4c559fe0fa15cb1de0b5fea2de9
Huan Doan <huandngoc@gmail.com>
2023-09-23 23:22:12 -0700

    Created WBC detection class

--- main.py
+++ main.py
@@ -1,38 +1,38 @@
+from collections import defaultdict
 from typing import Dict
 from ultralytics import YOLO
 from PIL import Image
 import tifffile
 import torch
-torch.cuda.set_device(0)
+from WhiteBloodCellDetector import WhiteBloodCellDetector
 
+# SETUP GPU
+device = "0" if torch.cuda.is_available() else "cpu"
+if device == "0":
+    torch.cuda.set_device(0)
+###
 
-def process__ndpi(ndpi):
-    wbc = {"N": 0, "L": 0, "M": 0, "E": 0, "B": 0}
-    rbc = 0
-    for h in ndpi_height:
-        for w in ndpi_width:
-            image = ndpi.crop(height, width, height + 512, width + 512)
-            wbc = wbc | count_wbcs(image)  # merge 2 dictionaries
-            rbc += count_rbc(image)
+# def process__ndpi(ndpi):
+#     wbc = {"N": 0, "L": 0, "M": 0, "E": 0, "B": 0}
+#     rbc = 0
+#     for h in ndpi_height:
+#         for w in ndpi_width:
+#             image = ndpi.crop(height, width, height + 512, width + 512)
+#             wbc = wbc | count_wbcs(image)  # merge 2 dictionaries
+#             rbc += count_rbc(image)
 
-    generate_summary(wbcs, rbc)
+#     generate_summary(wbcs, rbc)
 
 
 if __name__ == '__main__':
-    for ndpi in folder:
-        process__ndpi(ndpi)
+    wbcDetector = WhiteBloodCellDetector("models/wbc-model-Feb24.pt", "models/wbc-classification-box-Sep23.pt")
+    image = Image.open("samples/sample4.jpg")
+    wbcs = wbcDetector.detect(image)
+    print(wbcs)
+    # for ndpi in folder:
+    #     process__ndpi(ndpi)
 
 
-class WhiteBloodCellDetector:
-    def __init__(self, model_path):
-        self.model = YOLO(model_path)
 
-    def is_gpu(self) -> bool:
-        return self.model.device.type == "cuda"
 
-    def count(self, image: Image) -> Dict:
-        results = self.model(image)
-        for r in results:
-            # if gpu then convert to cpu
-            if self.is_gpu():
-                r.boxes = r.boxes.cpu()
+            
