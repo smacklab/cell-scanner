f5441fa9eba0176ccbf0956e9d349373632b065c
dnhuan <huandngoc@gmail.com>
2023-09-25 10:53:25 -0700

    Added density check

--- Detectors.py
+++ Detectors.py
@@ -1,12 +1,24 @@
 from collections import defaultdict
 from ultralytics import YOLO
 from PIL import Image
-class WhiteBloodCellDetector:
+
+
+class Singleton(type):
+    # https://stackoverflow.com/questions/6760685/creating-a-singleton-in-python
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
+        return cls._instances[cls]
+
+
+class WhiteBloodCellDetector(metaclass=Singleton):
     def __init__(self, detect_model_path, classify_model_path, device="cpu", DEBUG=False):
         self.CONFIDENCE_THRESHOLD = 0.25
         self.CLASSIFY_SIZE = 224
         self.DETECTION_SIZE = 512
-        self.IMAGE_SIZE_RATIO_THRESHOLD = 0.7
+        self.IMAGE_SIZE_RATIO_THRESHOLD = 0.5
         self.DEBUG = DEBUG
         self.device = device
         self.dmodel = YOLO(detect_model_path)
@@ -18,24 +30,24 @@ class WhiteBloodCellDetector:
     def detect(self, image: Image) -> defaultdict:
         wbcs = defaultdict(int)
 
-        r = self.dmodel(image, device=self.device, verbose=False)[0] # results always a list of length 1
+        r = self.dmodel(image, device=self.device, verbose=False)[0]  # results always a list of length 1
 
         if self.DEBUG:
-            im_array = r.plot() # plot wbcs
+            im_array = r.plot()  # plot wbcs
             im = Image.fromarray(im_array[..., ::-1])
             im.show()  # show image
 
         if self.is_gpu():
             r.boxes = r.boxes.cpu()
         r.boxes = r.boxes.numpy()
-
         for conf, xywh in zip(r.boxes.conf, r.boxes.xywh):
             _, _, width, height = xywh
-            if conf > self.CONFIDENCE_THRESHOLD and (self.IMAGE_SIZE_RATIO_THRESHOLD < width / height < 1 / self.IMAGE_SIZE_RATIO_THRESHOLD) :
+            if conf > self.CONFIDENCE_THRESHOLD and (self.IMAGE_SIZE_RATIO_THRESHOLD < width / height < 1 / self.IMAGE_SIZE_RATIO_THRESHOLD):
                 wbc_classname = self.classify(image, xywh)
+                print(wbc_classname)
                 wbcs[wbc_classname] += 1
-                
-        return wbcs
+
+        return dict(wbcs)
 
     def classify(self, image: Image, xywh) -> str:
         center_x, center_y, _, _ = xywh
@@ -50,22 +62,22 @@ class WhiteBloodCellDetector:
             r.boxes = r.boxes.cpu()
         r.boxes = r.boxes.numpy()
 
-        cords = [(x,y,w,h,cls) for (x,y,w,h), cls in zip(r.boxes.xywh, r.boxes.cls)]
+        cords = [(x, y, w, h, cls) for (x, y, w, h), cls in zip(r.boxes.xywh, r.boxes.cls)]
         closest = self.get_wbc_closest_to_center(cords)
         cls = closest[-1]
         return r.names[cls]
-    
+
     def get_wbc_closest_to_center(self, cords: list) -> tuple:
         center = (self.CLASSIFY_SIZE // 2, self.CLASSIFY_SIZE // 2)
         distances = list(map(lambda cord: self.distance(cord, center), cords))
         closest_index = distances.index(min(distances))
         return cords[closest_index]
-    
+
     def distance(self, p1: tuple, p2: tuple) -> float:
         return ((p1[0] - p2[0])**2 + (p1[1] - p2[1])**2)**0.5
-    
 
-class RedBloodCellDetector:
+
+class RedBloodCellDetector(metaclass=Singleton):
     def __init__(self, detect_model_path, device="cpu", DEBUG=False):
         self.model = YOLO(detect_model_path)
         self.CONFIDENCE_THRESHOLD = 0.25
@@ -79,10 +91,10 @@ class RedBloodCellDetector:
     def detect(self, image: Image) -> int:
         rbc = 0
 
-        r = self.model(image, device=self.device, verbose=False)[0] # results always a list of length 1
+        r = self.model(image, device=self.device, verbose=False)[0]  # results always a list of length 1
 
         if self.DEBUG:
-            im_array = r.plot(labels=False) # plot rbcs
+            im_array = r.plot(labels=False)  # plot rbcs
             im = Image.fromarray(im_array[..., ::-1])
             im.show()  # show image
 
@@ -92,7 +104,32 @@ class RedBloodCellDetector:
 
         for conf, xywh in zip(r.boxes.conf, r.boxes.xywh):
             _, _, width, height = xywh
-            if conf > self.CONFIDENCE_THRESHOLD and (self.IMAGE_SIZE_RATIO_THRESHOLD < width / height < 1 / self.IMAGE_SIZE_RATIO_THRESHOLD) :
+            if conf > self.CONFIDENCE_THRESHOLD and (self.IMAGE_SIZE_RATIO_THRESHOLD < width / height < 1 / self.IMAGE_SIZE_RATIO_THRESHOLD):
                 rbc += 1
-                
+
         return rbc
+
+
+class BloodDensityDetector(metaclass=Singleton):
+    def __init__(self, density_model_path, device="cpu", DEBUG=False):
+        self.model = YOLO(density_model_path)
+        self.device = device
+        self.DEBUG = DEBUG
+
+    def is_gpu(self) -> bool:
+        return self.device != "cpu"
+
+    def hasGoodDensity(self, image: Image) -> int:
+        r = self.model(image, device=self.device, verbose=False)[0]  # results always a list of length 1
+
+        if self.DEBUG:
+            im_array = r.plot(labels=False)  # plot density
+            im = Image.fromarray(im_array[..., ::-1])
+            im.show()  # show image
+
+        if self.is_gpu():
+            r.boxes = r.boxes.cpu()
+        cls_idx = r.probs.top1
+        cls_name = r.names[cls_idx]
+
+        return cls_name == "Good"
--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from collections import defaultdict
-from typing import Dict
+
+from typing import NamedTuple, Dict
 from ultralytics import YOLO
 from PIL import Image
 import tifffile
 import torch
-from Detectors import WhiteBloodCellDetector, RedBloodCellDetector
+from Detectors import WhiteBloodCellDetector, RedBloodCellDetector, BloodDensityDetector
 
 # SETUP GPU
 device = "0" if torch.cuda.is_available() else "cpu"
@@ -12,30 +12,36 @@ if device == "0":
     torch.cuda.set_device(0)
 ###
 
+ScanResult = NamedTuple("ScanResult", [("wbc", Dict), ("rbc", int)])
+
 # def process__ndpi(ndpi):
 #     wbc = {"N": 0, "L": 0, "M": 0, "E": 0, "B": 0}
 #     rbc = 0
 #     for h in ndpi_height:
 #         for w in ndpi_width:
 #             image = ndpi.crop(height, width, height + 512, width + 512)
-#             wbc = wbc | count_wbcs(image)  # merge 2 dictionaries
-#             rbc += count_rbc(image)
+#             process_image(image)
+
 
 #     generate_summary(wbcs, rbc)
 
+def process_image(image: Image) -> ScanResult:
+    bloodDensityDetector = BloodDensityDetector("models/blood-smear-density-Apr10.pt", DEBUG=True)
+
+    if not bloodDensityDetector.hasGoodDensity(image):
+        # scan is not good, return empty result
+        return ScanResult({}, 0)
+
+    wbcDetector = WhiteBloodCellDetector("models/wbc-detection-Feb24.pt", "models/wbc-classification-Sep23.pt", DEBUG=True)
+    rbcDetector = RedBloodCellDetector("models/rbc-detection-Sep12.pt", DEBUG=True)
+    wbc = wbcDetector.detect(image)
+    rbc = rbcDetector.detect(image)
+    return ScanResult(wbc, rbc)
+
 
 if __name__ == '__main__':
-    wbcDetector = WhiteBloodCellDetector("models/wbc-detection-Feb24.pt", "models/wbc-classification-Sep23.pt")
-    rbcDetector = RedBloodCellDetector("models/rbc-detection-Sep12.pt")
-    cropped_image = Image.open("samples/sample4.jpg")
-    wbcs = wbcDetector.detect(cropped_image)
-    rbc = rbcDetector.detect(cropped_image)
-    print(wbcs)
-    print(rbc)
+
+    cropped_image = Image.open("samples/sample1.jpg")
+    print(process_image(cropped_image))
     # for ndpi in folder:
     #     process__ndpi(ndpi)
-
-
-
-
-            
